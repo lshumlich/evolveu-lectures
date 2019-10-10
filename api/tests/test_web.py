629e2a4b059b99bed3e6f1a87f696844b56cab00
Larry Shumlich <lshumlich@gmail.com>
2019-10-09 22:00:31 -0600

    Build the API

--- api/tests/test_web.py
+++ api/tests/test_web.py
@@ -6,9 +6,12 @@ import pytest
 from flask import json
 import web
 
+calgary = {"key":1, "city":"Calgary", "lat":51.05, "long":-114.05}
+edmonton = {"key":2, "city":"Edmonton", "lat":53.55, "long":-113.49}
+redDeer = {"key":3, "city":"Red Deer", "lat":52.28, "long":-113.81}
+
 @pytest.fixture
 def client():
-
     web.app.config['TESTING'] = True
     client = web.app.test_client()
 
@@ -16,6 +19,83 @@ def client():
 
 
 def test_root(client):
-	rv = client.get('/')
-	assert(b'EvolveU test' in rv.data)
-	assert(b'API' in rv.data)
+    rv = client.get('/')
+    assert(rv.status_code == 200)
+    assert(b'EvolveU test' in rv.data)
+    assert(b'API' in rv.data)
+
+
+def test_crud(client):
+    # we should start with no data
+    rv = client.post('/all')
+    assert(rv.status_code == 200)
+    json = rv.get_json()
+    assert(len(json) == 0)
+
+    # add one
+    rv = client.post('/add', 
+        json=calgary)
+    assert(rv.status_code == 200)
+
+    # we should have one now
+    rv = client.post('/all')
+    assert(rv.status_code == 200)
+    json = rv.get_json()
+    assert(len(json) == 1)
+
+    # add a second one
+    rv = client.post('/add', 
+        json=edmonton)
+    assert(rv.status_code == 200)
+
+    # we should have two now
+    rv = client.post('/all')
+    assert(rv.status_code == 200)
+    json = rv.get_json()
+    assert(len(json) == 2)
+
+    # delete edmonton and there should be one
+    rv = client.post('/delete', 
+        json={"key": edmonton["key"]})
+    assert(rv.status_code == 200)
+
+    # we should have one now
+    rv = client.post('/all')
+    assert(rv.status_code == 200)
+    json = rv.get_json()
+    assert(len(json) == 1)
+
+    # delete edmonton again and there should still be one
+    rv = client.post('/delete', 
+        json={"key": edmonton["key"]})
+    # the delete should fail
+    assert(rv.status_code == 404)
+
+    # we should have one now
+    rv = client.post('/all')
+    assert(rv.status_code == 200)
+    json = rv.get_json()
+    assert(len(json) == 1)
+
+    # delete calgary and there should zero
+    rv = client.post('/delete', 
+        json={"key": calgary["key"]})
+    assert(rv.status_code == 200)
+
+    # they should be all gone
+    rv = client.post('/all')
+    assert(rv.status_code == 200)
+    json = rv.get_json()
+    assert(len(json) == 0)
+
+
+
+def test_test(client):
+    # data or json can be sent but not both
+    rv = client.post('/test', 
+        # data=dict(fname="Larry", lname="Shumlich"), 
+        json={"key1":"value1","key2":"value2"})
+    assert(rv.status_code == 200)
+    # print('test_test rv: ',rv)
+    # print('test_test rv.data: ',rv.data)
+    # print('test_test rv.json: ',rv.get_json())
--- api/web.py
+++ api/web.py
@@ -2,8 +2,13 @@
 # See README.md for instructions
 #
 import os
-from flask import Flask, jsonify
+import traceback
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+
+# cities = [{"city":"Calgary", "lat":52.345, "long":60.345}]
+cities = []
+keys = {}
 
 print("--- Starting", __file__)
 
@@ -13,7 +18,69 @@ CORS(app, supports_credentials=True)
 
 @app.route("/")
 def hello():
-	# session["lfs"] = "Larry"
-	# s = escape(str(session))
-	# print("Session for this dude:", s)
 	return "<h1>EvolveU test</h1> <h2>API Server up and running..</h2>"
+
+
+@app.route("/all", methods = ['POST','GET'])
+def all():
+	try :
+		content = request.get_json()
+		return jsonify(cities), 200
+	except Exception as e:
+		traceback.print_stack()
+		print('**** Not a valid request. ', e)
+
+	return jsonify('{}'), 404
+
+#
+# There is a bug here. That same key can be added multiple times but can not
+# be deleted..... Before you fix the code what do you need to do first?
+#
+@app.route("/add", methods = ['POST'])
+def add():
+	try :
+		content = request.get_json()
+		keys[content["key"]] = len(cities)
+		# print(keys)
+		cities.append(content)
+		return jsonify({}), 200
+
+	except Exception as e:
+		traceback.print_stack()
+		print('**** Not a valid request. ', e)
+
+	return jsonify('{}'), 404
+
+@app.route("/delete", methods = ['POST'])
+def delete():
+	try :
+		content = request.get_json()
+		key = content["key"]
+		cities.pop(keys[key])
+		del keys[key]
+
+		return jsonify({}), 200
+
+	except Exception as e:
+		# traceback.print_stack()
+		print('**** Not a valid request. ', e)
+
+	return jsonify('{}'), 404
+
+
+@app.route("/test", methods = ['POST','GET'])
+def test():
+	try :
+		content = request.get_json()
+		# print('in /test request: ',request)
+		# print('in /test path: ',request.path)
+		# print('in /test form: ',request.form)
+		# print('in /test parms: ',request.args)
+		# print('in /test json: ',request.get_json())
+		return jsonify({'status': 'ok'}), 200
+
+	except Exception as e:
+		traceback.print_stack()
+		print('**** Not a valid request. ', e)
+
+	return jsonify('{}'), 404
